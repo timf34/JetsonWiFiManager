bdf26b20b673215d461fafc0a0ac6d1cfa5d342c
timf34 <farrelti@tcd.ie>
2023-06-15 11:45:38 +0100

    updated Flask server which starts network manager once the ssid and password have been inputted

--- server.py
+++ server.py
@@ -1,35 +1,41 @@
-from flask import Flask, request
+from flask import Flask, render_template, request
 import os
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
-def form():
-    if request.method == 'POST':
-        ssid = request.form.get('ssid')
-        ssid = request.form.get('ssid')
-        password = request.form.get('password')
 
-        # Save the SSID and password to a file
-        with open('wifi_credentials.txt', 'w') as f:
-            f.write(f'{ssid}\n{password}')
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-        # Use the submitted SSID and password to connect to the new network
-        # You would replace 'wlan0' with the actual interface you want to use to connect to the new network
-        os.system(f'sudo nmcli dev wifi con "{ssid}" password "{password}" ifname wlan0')
 
-        return 'Success! Attempting to connect...'
+@app.route('/postmethod', methods = ['POST'])
+def get_post_javascript_data() -> str:
+    jsdata = request.form['javascript_data']
+    ssid, password = jsdata.split("|")
+    with open('wifi_config.txt', 'w') as f:
+        f.write(ssid + "\n" + password)
 
-    # Serve the form
-    return '''
-        <form method="POST">
-            SSID: <br>
-            <input type="text" name="ssid"><br>
-            Password: <br>
-            <input type="password" name="password"><br>
-            <input type="submit" value="Submit">
-        </form>
-    '''
+    connect_to_wifi()
+    return jsdata
+
+
+def connect_to_wifi() -> None:
+    with open('wifi_config.txt', 'r') as f:
+        lines = f.readlines()
+        ssid = lines[0].strip()
+        password = lines[1].strip()
+
+    # stop the AP
+    os.system('sudo systemctl stop dnsmasq')
+    os.system('sudo systemctl stop hostapd')
+
+    # restart NetworkManager
+    os.system('sudo systemctl start NetworkManager')
+
+    # try to connect to WiFi
+    os.system(f'nmcli d wifi connect {ssid} password {password}')
+
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
+    app.run(host= '0.0.0.0')
